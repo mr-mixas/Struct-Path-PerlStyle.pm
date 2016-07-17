e3f4b6e2b029647a3a38f2625ef8a81af79cae28
mr-mixas <mixas.sr@gmail.com>
2016-07-17 20:20:20 +0300

    0.23

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Struct::Path::PerlStyle - Perl-style syntax frontend for [Struct::Path](https://
 
 # VERSION
 
-Version 0.22
+Version 0.23
 
 # SYNOPSIS
 
--- lib/Struct/Path/PerlStyle.pm
+++ lib/Struct/Path/PerlStyle.pm
@@ -16,11 +16,11 @@ Struct::Path::PerlStyle - Perl-style syntax frontend for L<Struct::Path|Struct::
 
 =head1 VERSION
 
-Version 0.22
+Version 0.23
 
 =cut
 
-our $VERSION = '0.22';
+our $VERSION = '0.23';
 
 =head1 SYNOPSIS
 
